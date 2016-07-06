5d8786d78ac1d82ddd87810b6cf2e7ac5f66a657
NicolasGodfather <godfather6worlds@gmail.com>
2016-07-06 17:23:56 +0300

    make rule

---
+++ src/main/java/com/filtering/Rule.java
@@ -0,0 +1,38 @@
+package com.filtering;
+
+/**
+ * Realization Rule like entity
+ *
+ * @author Nicolas Asinovich.
+ */
+public class Rule {
+
+    private RuleType ruleType;
+
+    private int weight;
+
+    public Rule (RuleType ruleType, int weight) {
+        this.ruleType = ruleType;
+        this.weight = weight;
+    }
+
+    public RuleType getRuleType () {
+        return ruleType;
+    }
+
+    public void setRuleType (RuleType ruleType) {
+        this.ruleType = ruleType;
+    }
+
+    public int getWeight () {
+        return weight;
+    }
+
+    public void setWeight (int weight) {
+        if (weight > 0) {
+            this.weight = weight;
+        } else {
+            throw new IllegalArgumentException("The weight should be a positive integer!");
+        }
+    }
+}
---
+++ src/main/java/com/filtering/RuleType.java
@@ -0,0 +1,20 @@
+package com.filtering;
+
+/**
+ * Realization types of rule
+ *
+ * @author Nicolas Asinovich.
+ */
+enum RuleType {
+    CHILD(1), SUB(2), ROOT(3);
+
+    private final int ruleTypePrecedence;
+
+    RuleType (int ruleTypePrecedence) {
+        this.ruleTypePrecedence = ruleTypePrecedence;
+    }
+
+    public int getRuleTypePrecedence () {
+        return ruleTypePrecedence;
+    }
+}
